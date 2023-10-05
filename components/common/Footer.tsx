d94a8ba5ce781221a97687fe36ead3b5ebdd2f13
Andrés Aparicio <andres.apa.23@gmail.com>
2023-10-04 20:07:07 -0500

    Footer design

--- components/common/Footer.tsx
+++ components/common/Footer.tsx
@@ -8,30 +8,64 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 function Footer() {
   return (
-    <footer className='flex justify-around'>
-      <section id='footer-menu'>
+    <footer className='flex justify-around bg-gray-500 text-white py-8'>
+      <section
+        id='footer-menu'
+        className='flex-1 flex justify-center items-center'
+      >
         <nav className='flex'>
-          <ul>
-            <li>Menu option</li>
-            <li>Menu option</li>
-            <li>Menu option</li>
-          </ul>
-          <ul>
-            <li>Menu option</li>
-            <li>Menu option</li>
-            <li>Menu option</li>
-          </ul>
+          <div>
+            <h2 className='text-xl font-semibold'>Explora</h2>
+            <hr className='solid mb-2' />
+            <ul className='leading-6'>
+              <li>
+                <a href='#'>Home</a>
+              </li>
+              <li>
+                <a href='#'>Cerca de ti</a>
+              </li>
+              <li>
+                <a href='#'>Red de trabajo</a>
+              </li>
+            </ul>
+          </div>
+          <div className='ml-8'>
+            <h2 className='text-xl font-semibold'>Información</h2>
+            <hr className='solid mb-2' />
+            <ul className='leading-6'>
+              <li>
+                <a href='#'>Tu perfil</a>
+              </li>
+              <li>
+                <a href='#'>Pedidos en curso</a>
+              </li>
+              <li>
+                <a href='#'>Contáctanos</a>
+              </li>
+            </ul>
+          </div>
         </nav>
       </section>
-      <section>
-        <h2>Siguenos</h2>
-        <div id='social-icons' className='flex'>
-          <FontAwesomeIcon icon={faFacebook} height={25} />
-          <FontAwesomeIcon icon={faInstagram} height={25} />
-          <FontAwesomeIcon icon={faTwitter} height={25} />
+      <section className='flex-1 flex justify-center items-center'>
+        <div className='h-fit'>
+          <img
+            className='h-16 mx-auto'
+            src='/assets/iLunch-logo.png'
+            alt='iLunch logo'
+          />
+          <p className='text-sm text-center'>
+            © [2023] iLunch. Todos los derechos reservados.
+          </p>
         </div>
       </section>
-      <section>Aviso legal</section>
+      <section className='flex-1 flex items-center justify-center flex-col'>
+        <h2 className='text-xl font-semibold'>Siguenos</h2>
+        <div id='social-icons' className='flex mt-4 text-2xl'>
+          <FontAwesomeIcon icon={faFacebook} />
+          <FontAwesomeIcon className='mx-2' icon={faInstagram} />
+          <FontAwesomeIcon icon={faTwitter} />
+        </div>
+      </section>
     </footer>
   )
 }
