61f892a02efd57e51ab920531d8502c1a07b8ec5
mgallegoj <92808245+mgallegoj@users.noreply.github.com>
2023-10-08 14:44:41 -0500

    more about job offer

--- app/jobs/details/page.tsx
+++ app/jobs/details/page.tsx
@@ -45,6 +45,85 @@ export default function JobOffer({ params }: { params: { restaurantId: string } 
           <h1 className='text-xl font-semibold p-2'>¿Tienes alguna pregunta?</h1>
             <textarea className='w-full rounded-lg h-full resize-none' placeholder="Cuentanos tus dudas y nos pondremos en contacto contigo pronto"></textarea>
         </section>
+        <section className="p-5 grid grid-cols-1 sm:grid-cols-3">
+        <div className="p-4 rounded-lg border border-gray-500 mr-2 h-full">
+          <h1 className='text-lg font-semibold pb-2'>Horario Laboral</h1>
+          <table className="text-md p-2  w-full">
+            <tbody>
+              <tr>
+                <td className="align-top w-1/2">Lunes</td>
+                <td>08:00 - 17:00</td>
+              </tr>
+              <tr>
+                <td className="align-top ">Martes</td>
+                <td>08:00 - 17:00</td>
+              </tr>
+              <tr>
+                <td className="align-top ">Miércoles</td>
+                <td>08:00 - 17:00</td>
+              </tr>
+              <tr>
+                <td className="align-top ">Jueves</td>
+                <td>08:00 - 17:00</td>
+              </tr>
+              <tr>
+                <td className="align-top ">Viernes</td>
+                <td>08:00 - 17:00</td>
+              </tr>
+              <tr>
+                <td className="align-top ">Sábado</td>
+                <td>09:00 - 14:00</td>
+              </tr>
+              <tr>
+                <td className="align-top ">Domingo</td>
+                <td>Cerrado</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+
+
+
+          <div>
+            <div className="rounded-lg border border-gray-500">
+              <div className='p-2'>
+                <h1 className='text-lg font-semibold'>Encuentranos</h1>
+                <p className='text-sm '>Cra x # xxx - xxx local xx</p>
+              </div>
+            </div>
+            <div className="rounded-lg border border-gray-500 bg-cover bg-center mt-2">
+              <div className="h-[400px] bg-cover bg-center mt-2" style={{backgroundImage: `url('/assets/map-mockup.png')`}}>
+              
+              </div>
+            </div>
+          </div>
+          <div className="h-64 overflow-hidden rounded-lg border border-gray-500 ml-2 h-full w-full">
+            <div className="p-4">
+              <h1 className='text-lg font-semibold pb-2'>Sobre XXX-Restaurante</h1>
+              <table className="text-md w-full"> 
+                <tbody>
+                  <tr>
+                    <td className="align-top w-1/3">Info 1</td>
+                    <td>xxxxx xxxxxxxx xxxxxxxxx xxxxxxxxxxx xxxxxxx xx</td>
+                  </tr>
+                  <tr>
+                    <td className="align-top">Info 1</td>
+                    <td>xxxxxxxxx xxxxxxx xxxxxx xxxxxx xxxxx</td>
+                  </tr>
+                  <tr>
+                    <td className="align-top">Info 1</td>
+                    <td>xxxxxxxxxxxx xxxx xxxxxxxx xxxxxxxx xxxxxxxxxx xxxxxxxxxxxxxx</td>
+                  </tr>
+                  <tr>
+                    <td className="align-top">Info 1</td>
+                    <td>xxxxxxxxxxxx xxxx xxxxxxxx xxxxxxxx xxxxxxxxxx xxxxxxxxxxxxxx</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+
+        </section>
       </main>
     </>
   )
