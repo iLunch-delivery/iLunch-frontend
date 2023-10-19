d4b9ba6b3f9450936e12774c2dfcb7d204cff3e4
Jose <80296654+Dajovih@users.noreply.github.com>
2023-10-18 22:46:48 -0500

    Image  aligned with title text

--- app/register/candidate/page.tsx
+++ app/register/candidate/page.tsx
@@ -11,7 +11,7 @@ function Input({ texto = '', id, tipo = 'text' }) {
 export default function RegisterUser() {
   return (
     <main className='flex flex-col items-center p-0'>
-      <div className='inline-flex mt-10 align-middle font-semibold text-2xl'> Bienvenido a la red de trabajo de <Image src={logo} alt='iLunch logo' className='align-middle h-auto w-auto mx-auto my-auto'/></div>
+      <div className='flex flex-row mt-10 align-middle font-semibold text-2xl'> Bienvenido a la red de trabajo de   <Image src={logo} alt='iLunch logo' className='align-middle h-9 w-auto ml-2 '/></div>
       <div className='flex flex-col items-center mt-5'>
         <span>Por favor ingresa tus datos y cúentanos tu experiencia en el sector gastronómico.</span>
         <span className='mt-0'>También nos gustaría que anexaras tu hoja de vida y otros documentos que soporten tu experiencia y nos dejen saber más de ti.</span>
