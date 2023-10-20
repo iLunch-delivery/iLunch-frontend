52fefbb602797c8d4d58371ef11a1e1a1a81d210
David Mateo García Vallejo <davgarciava@unal.edu.co>
2023-10-19 20:25:17 -0500

    Dropdown fix

--- components/common/Dropdown.tsx
+++ components/common/Dropdown.tsx
@@ -14,7 +14,7 @@ export default function Dropdown({
       <button
         id='dropdownDefaultButton'
         data-dropdown-toggle='dropdown'
-        className='border border-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center'
+        className='border border-gray-300 font-medium w-full rounded-full text-sm p-2.5 text-center inline-flex items-center justify-between'
         type='button'
         onClick={() => {
           setIsOpen(!isOpen)
