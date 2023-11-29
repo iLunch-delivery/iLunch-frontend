f73f4e7a947b47ead5dbff58f8c6ac49ae184d18
Andrés Aparicio <andres.apa.23@gmail.com>
2023-11-29 10:43:31 -0500

    Add API call for login functionality

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -7,11 +7,11 @@ import mobile from '@/public/assets/Mobile.png'
 import android from '@/public/assets/Android-I.png'
 import apple from '@/public/assets/Apple - I.png'
 import google from '@/public/assets/google-logo.svg'
-import { userData } from '@/config/data/users'
 import { useUserInfo } from '@/contexts/UserInfoContext'
 import { useRouter } from 'next/navigation'
 import { ROLE } from '@/config/enums'
 import Link from 'next/link'
+import apiRoutes from '@/config/apiRoutes'
 
 export default function Login() {
   const router = useRouter()
@@ -29,13 +29,18 @@ export default function Login() {
     setSpeciality
   } = useUserInfo()
 
-  const handleLogin = (e: React.FormEvent<HTMLFormElement>) => {
+  const handleLogin = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
-    const userInfo = userData.find((user) => {
-      if (user.email === inputEmail && user.password === password) {
-        return user
+
+    const response = await fetch(apiRoutes.login, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
       }
     })
+
+    const userInfo = await response.json()
+
     if (userInfo != null) {
       setIsLogged(true)
       setAddress(userInfo.address)
@@ -55,14 +60,18 @@ export default function Login() {
   }
   return (
     <main className='flex h-full w-full p-0'>
-      <section className='form-section bg-blue-700 w-1/2 flex flex-col flex-1 items-center justify-around py-2'> {/* Se crea la sección del formulario para logearse */ }
+      <section className='form-section bg-blue-700 w-1/2 flex flex-col flex-1 items-center justify-around py-2'>
+        {' '}
+        {/* Se crea la sección del formulario para logearse */}
         <Image src={logo} alt='iLunch Logo' className='h-24 w-auto' />
         <form
           className=' flex flex-col w-1/2'
           onSubmit={(e) => {
-            handleLogin(e)
+            void handleLogin(e)
           }}
-        > {/* Se comprueba que los datos ingresados sean los correctos */}
+        >
+          {' '}
+          {/* Se comprueba que los datos ingresados sean los correctos */}
           <input
             type='email'
             placeholder='Correo electrónico'
@@ -71,7 +80,8 @@ export default function Login() {
             onChange={(e) => {
               setInputEmail(e.target.value)
             }}
-          /> {/* Se guarda el email para comprobación de información correcta de login */}
+          />{' '}
+          {/* Se guarda el email para comprobación de información correcta de login */}
           <input
             type='password'
             placeholder='Contraseña'
@@ -80,7 +90,8 @@ export default function Login() {
             onChange={(e) => {
               setPassword(e.target.value)
             }}
-          /> {/* Se guarda la contraseña para comprobación de información correcta de login */}
+          />{' '}
+          {/* Se guarda la contraseña para comprobación de información correcta de login */}
           <button
             type='submit'
             className=' mt-10 p-2 text-white rounded-full border-2 border-white sm:w-full lg:w-1/2  self-center'
@@ -94,12 +105,13 @@ export default function Login() {
               loading='lazy'
               alt='google logo'
             />
-            Ingresa con google {/* El inicio de sesión con google será implemetando después */}
+            Ingresa con google{' '}
+            {/* El inicio de sesión con google será implemetando después */}
           </button>
         </form>
         <div className=' text-white flex flex-col items-center'>
           <p className='flex text-center'>¿Aún no estás registrado?</p>
-          <Link 
+          <Link
             className=' mt-3 p-2 text-white text-center rounded-full border-2 self-center bg-orange-600 border-orange-600 sm:w-full lg:w-fit shadow-lg '
             href='/register/user'
           >
@@ -107,7 +119,9 @@ export default function Login() {
           </Link>
         </div>
       </section>
-      <section className='banner-section w-1/2 flex-col items-center justify-end flex-1 hidden md:flex'>  {/* Sección publicitaria de la app. La appa no existe en realidad. No se da prioridad en baja resoluciones debbido a que lo importante es logearse */}
+      <section className='banner-section w-1/2 flex-col items-center justify-end flex-1 hidden md:flex'>
+        {' '}
+        {/* Sección publicitaria de la app. La appa no existe en realidad. No se da prioridad en baja resoluciones debbido a que lo importante es logearse */}
         <Image className='h-2/3 w-auto' src={mobile} alt='Mobile App' />
         <div className='mt-8'>
           <h2 className=' font-semibold text-xl text-blue-700 '>
